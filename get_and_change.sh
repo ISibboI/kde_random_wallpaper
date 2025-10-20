3ba46190ff8a268692731d9da20ae37f766775e3
Sebastian Schmidt <isibboi@gmail.com>
2025-10-20 10:33:10 +0300

    Use wallpapers from local folder instead of internet.

--- get_and_change.sh
+++ get_and_change.sh
@@ -1,26 +1,10 @@
 #!/bin/bash
 #
-# Author: KZKG^Gaara (https://x.com/kzkggaara)
-#
-# Requirements:
-# - KDE Plasma
-# - wget
-# - curl
-# 
-# What does this script do?:
-# 1. Check if you are using KDE [line 18]
-# 2. Get a random number [line 20]
-# 3. Set your screen resolution [line 21]
-# 3. Download to /tmp/ some random wallpaper [line 23]
-# 4. Put the wallpaper as default in all your desktops [line 25-34]
-# 5. Delete the old one downloaded last time [line 36]
+# Original by: KZKG^Gaara (https://x.com/kzkggaara)
 
 if pgrep -x "plasmashell" >/dev/null; then
 
-    RANDOM_NUMBER=$RANDOM
-    SCREEN_RESOLUTION="1920x1080" 
-
-    wget $(curl -s --location --request GET 'https://wallhaven.cc/api/v1/search' --header 'Content-Type: application/json' --data-raw '{"sorting": "random","resolutions": "'$SCREEN_RESOLUTION'"}' | jq .data | grep "path" | head -n 1 | awk {'print $2'} | sed 's|"||g' | sed 's|,||g') -O /tmp/random_wallpaper_$RANDOM_NUMBER -o /dev/null
+    WALLPAPER=$(find /data/Wallpapers -type f | shuf -n 1)
 
     dbus-send --session --dest=org.kde.plasmashell --type=method_call /PlasmaShell org.kde.PlasmaShell.evaluateScript 'string:
     var Desktops = desktops();                                                                                                                       
@@ -30,10 +14,8 @@ if pgrep -x "plasmashell" >/dev/null; then
         d.currentConfigGroup = Array("Wallpaper",
             "org.kde.image",
             "General");
-        d.writeConfig("Image", "file:///tmp/random_wallpaper_'$RANDOM_NUMBER'");
+        d.writeConfig("Image", "file://'$WALLPAPER'");
     }'
-
-    ls -d /tmp/random_wallpaper* | grep -v $RANDOM_NUMBER | xargs rm
 
 else
     echo ""
